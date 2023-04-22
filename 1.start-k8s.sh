09f5267e199f3d3f9803e148dc7b228748780283
Mokhlesur Rahman <mokhlesurr031@gmail.com>
2023-04-22 21:21:50 +0600

    deployment k8s moved, script updated

--- 1.start-k8s.sh
+++ 1.start-k8s.sh
@@ -3,23 +3,26 @@
 echo "Starting the cluster..."
 echo "-----------------------"
 
-kubectl apply -f k8s/psql/0-namespace-psql.yaml && 
-kubectl apply -f k8s/psql/1-configmap-psql.yaml &&
-kubectl apply -f k8s/psql/2-secret-psql.yaml &&
-kubectl apply -f k8s/psql/3-persistent-volume-psql.yaml &&
-kubectl apply -f k8s/psql/4-persistent-volume-claim-psql.yaml &&
-kubectl apply -f k8s/psql/5-statefulset-psql.yaml &&
-kubectl apply -f k8s/psql/6-service-psql.yaml &&
+kubectl apply -f deployment/k8s/psql/0-namespace-psql.yaml && 
+kubectl apply -f deployment/k8s/psql/1-configmap-psql.yaml &&
+kubectl apply -f deployment/k8s/psql/2-secret-psql.yaml &&
+kubectl apply -f deployment/k8s/psql/3-persistent-volume-psql.yaml &&
+kubectl apply -f deployment/k8s/psql/4-persistent-volume-claim-psql.yaml &&
+kubectl apply -f deployment/k8s/psql/5-statefulset-psql.yaml &&
+kubectl apply -f deployment/k8s/psql/6-service-psql.yaml &&
 
 echo "Postgresql statefulset is up and running"
 echo "-----------------------"
 
-kubectl apply -f k8s/client/0-namespace-client.yaml &&
-kubectl apply -f k8s/client/1-deployment-client.yaml &&
-kubectl apply -f k8s/client/2-service-client.yaml &&
-kubectl apply -f k8s/client/3-ingress-client.yaml &&
+kubectl apply -f deployment/k8s/client/0-namespace-client.yaml &&
+kubectl apply -f deployment/k8s/client/1-deployment-client.yaml &&
+kubectl apply -f deployment/k8s/client/2-service-client.yaml &&
+kubectl apply -f deployment/k8s/client/3-ingress-client.yaml &&
 
 echo "Client deployment is up and running"
+
+echo "Waiting for 5 seconds..."
+sleep 5
 
 echo "Starting port forwarding..."
 echo "--------------------------"
--- 2.destroy-k8s.sh
+++ 2.destroy-k8s.sh
@@ -3,17 +3,17 @@
 echo "Deleting the cluster..."
 echo "-----------------------"
 
-kubectl delete -f k8s/client/3-ingress-client.yaml &&
-kubectl delete -f k8s/client/2-service-client.yaml &&
-kubectl delete -f k8s/client/1-deployment-client.yaml &&
-kubectl delete -f k8s/client/0-namespace-client.yaml &&
+kubectl delete -f deployment/k8s/client/3-ingress-client.yaml &&
+kubectl delete -f deployment/k8s/client/2-service-client.yaml &&
+kubectl delete -f deployment/k8s/client/1-deployment-client.yaml &&
+kubectl delete -f deployment/k8s/client/0-namespace-client.yaml &&
 
-kubectl delete -f k8s/psql/6-service-psql.yaml &&
-kubectl delete -f k8s/psql/5-statefulset-psql.yaml &&
-kubectl delete -f k8s/psql/4-persistent-volume-claim-psql.yaml &&
-kubectl delete -f k8s/psql/3-persistent-volume-psql.yaml &&
-kubectl delete -f k8s/psql/2-secret-psql.yaml &&
-kubectl delete -f k8s/psql/1-configmap-psql.yaml &&
-kubectl delete -f k8s/psql/0-namespace-psql.yaml &&
+kubectl delete -f deployment/k8s/psql/6-service-psql.yaml &&
+kubectl delete -f deployment/k8s/psql/5-statefulset-psql.yaml &&
+kubectl delete -f deployment/k8s/psql/4-persistent-volume-claim-psql.yaml &&
+kubectl delete -f deployment/k8s/psql/3-persistent-volume-psql.yaml &&
+kubectl delete -f deployment/k8s/psql/2-secret-psql.yaml &&
+kubectl delete -f deployment/k8s/psql/1-configmap-psql.yaml &&
+kubectl delete -f deployment/k8s/psql/0-namespace-psql.yaml &&
 
 echo "Cluster resources have been deleted."
